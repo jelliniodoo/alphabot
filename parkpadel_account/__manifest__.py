0f859fbdf69ffca15af7bbbbf4c0ea827b100048
jelliniodoo <101937320+jelliniodoo@users.noreply.github.com>
2022-03-19 12:27:38 -0500

    Add files via upload

---
+++ parkpadel_account/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Park Padel - Panama - Accounting",
+    "description": """
+Panamenian accounting chart and tax localization.
+
+Plan contable panameño e impuestos de acuerdo a disposiciones vigentes
+
+    """,
+    "author": "Alphapos",
+    'category': 'Accounting/Localizations/Account Charts',
+    "depends": ["account"],
+    "data": [
+        "data/parkpadel_chart_data.xml",
+        "data/account_data.xml",
+        "data/account_tax_data.xml",
+        "data/account_chart_template_data.xml",
+    ],
+    'license': 'LGPL-3',
+}
